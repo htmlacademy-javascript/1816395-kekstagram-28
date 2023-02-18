744ffc809df2da6bb578479c684f4bc9baa6dfa4
evilJO <eviljosephe@gmail.com>
2023-02-18 19:57:39 +0200

    ficha добавлена функция извлечени числа из строки

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,16 @@ function testPolindrom(str){
   str===reversStr? testResult=true : testResult=false;
   return testResult
 }
+
+function cutNumber(data){
+  data=String(data).split('');
+  const numberArrey=[0,1,2,3,4,5,6,7,8,9];
+  let cutNumberResult=[];
+  for (let i =0; i<data.length;i++){
+    if (numberArrey.includes(Number(data[i]))){
+      cutNumberResult.push(data[i]);
+    };
+
+  }
+  return Number(cutNumberResult.join(''));
+}
