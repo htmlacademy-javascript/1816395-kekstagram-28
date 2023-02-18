00dcf1fbe60dcc4071d61c842fbc2bd5a49d9e77
evilJO <eviljosephe@gmail.com>
2023-02-18 19:05:26 +0200

    ficha добавлен тест на полиндром

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,11 @@
 function testString(string,stringLength){
-  let testResult
   string.length<=stringLength?  testResult=true : testResult=false;
+  return testResult
+}
+
+function testPolindrom(str){
+  str=str.toLowerCase().replaceAll(' ','');
+  let reversStr=str.split('').reverse().join('');
+  str===reversStr? testResult=true : testResult=false;
   return testResult
 }
