21fe03ad6e537f7393ff672b0eee475a0be30f9f
evilJO <eviljosephe@gmail.com>
2023-02-19 23:55:51 +0200

    ficha добавлена 4я функция

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,76 @@
-function testString(string,stringLength){
-  string.length <= stringLength ? testResult = true : testResult = false;
-  return testResult;
+function getTestString(string,stringLength){
+  let strResult;
+  string.length <= stringLength ? strResult = true : strResult = false;
+  return strResult;
 }
 
-function testPolindrom(str){
+function getTestPolindrom(str){
+  let testResult;
   str = str.toLowerCase().replaceAll(' ','');
   const reversStr = str.split('').reverse().join('');
   str === reversStr ? testResult = true : testResult = false;
   return testResult;
 }
 
-function cutNumber(data){
+function getCutNumber(data){
   data = String(data).split('');
-  const numberArrey = [0,1,2,3,4,5,6,7,8,9];
-  const cutNumberResult = [];
+  const NumberArrey = [0,1,2,3,4,5,6,7,8,9];
+  const CutNumberResult = [];
   for (let i = 0; i < data.length;i++){
-    if (numberArrey.includes(Number(data[i]))){
-      cutNumberResult.push(data[i]);
+    if (NumberArrey.includes(Number(data[i]))){
+      CutNumberResult.push(data[i]);
     }
-
   }
-  return Number(cutNumberResult.join(''));
+  return Number(CutNumberResult.join(''));
 }
+
+// function getAddStr (str,minLength,addStr){
+//   if (str.length < minLength){
+//     let resultAddStr = '';
+//     if(addStr.length >= minLength){
+//       resultAddStr = addStr.slice(0,minLength - str.length) + str;
+//       return resultAddStr;
+//     } else {
+//       let indexOfAddStr = 0;
+//       for (let i = 0 ; i < minLength - str.length; i++){
+//         if (addStr[indexOfAddStr] === undefined){
+//           indexOfAddStr = 0;
+//         }
+//         resultAddStr = resultAddStr + addStr[indexOfAddStr];
+//         indexOfAddStr++;
+//       }
+//       resultAddStr = resultAddStr + str;
+//       return resultAddStr;
+//     }
+//   } else {
+//     return str;
+//   }
+// }
+
+function getAddStr (str,minLength,addStr){
+  if (str.length < minLength){
+    let resultAddStr = '';
+    if(addStr.length >= minLength){
+      resultAddStr = addStr.slice(0,minLength - str.length) + str;
+      return resultAddStr;
+    } else {
+      while(resultAddStr.length < minLength - str.length){
+        resultAddStr = addStr + resultAddStr;
+        if(minLength - resultAddStr.length <= addStr.length){
+          break;
+        }
+      }
+      const lineFill = (minLength - (resultAddStr.length + str.length));
+      if (lineFill !== 0){
+        const lastPartResult = (minLength - str.length) % addStr.length;
+        resultAddStr = addStr.slice(0,lastPartResult) + resultAddStr;
+      }
+      resultAddStr = resultAddStr + str;
+      return resultAddStr;
+    }
+  } else {
+    return str;
+  }
+}
+
+//  про соблюдение критериев страшно даже подумать...Т_Т
