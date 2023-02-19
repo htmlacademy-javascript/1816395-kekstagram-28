bbb30f76c64809aa6ad40d6550f63de99df57c2d
evilJO <eviljosephe@gmail.com>
2023-02-19 20:02:38 +0200

    fix попытка избаситься от ошибок

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,23 @@
 function testString(string,stringLength){
-  string.length<=stringLength?  testResult=true : testResult=false;
-  return testResult
+  string.length <= stringLength ? testResult = true : testResult = false;
+  return testResult;
 }
 
 function testPolindrom(str){
-  str=str.toLowerCase().replaceAll(' ','');
-  let reversStr=str.split('').reverse().join('');
-  str===reversStr? testResult=true : testResult=false;
-  return testResult
+  str = str.toLowerCase().replaceAll(' ','');
+  const reversStr = str.split('').reverse().join('');
+  str === reversStr ? testResult = true : testResult = false;
+  return testResult;
 }
 
 function cutNumber(data){
-  data=String(data).split('');
-  const numberArrey=[0,1,2,3,4,5,6,7,8,9];
-  let cutNumberResult=[];
-  for (let i =0; i<data.length;i++){
+  data = String(data).split('');
+  const numberArrey = [0,1,2,3,4,5,6,7,8,9];
+  const cutNumberResult = [];
+  for (let i = 0; i < data.length;i++){
     if (numberArrey.includes(Number(data[i]))){
       cutNumberResult.push(data[i]);
-    };
+    }
 
   }
   return Number(cutNumberResult.join(''));
